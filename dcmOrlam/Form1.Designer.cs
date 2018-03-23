a83b0a51c55a8143a2b56ea5baa646ad8f24c781
Unknown <pierre8.g@gmail.com>
2018-03-23 11:14:30 +0100

    programmev3 affichage console + affichage erreur

---
+++ dcmOrlam/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace dcmOrlam
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		/// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Code généré par le Concepteur Windows Form
+
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.button_VMAT = new System.Windows.Forms.Button();
+			this.button_AutoBreast = new System.Windows.Forms.Button();
+			this.textBox_result = new System.Windows.Forms.TextBox();
+			this.status = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.textConsole = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button_VMAT
+			// 
+			this.button_VMAT.Location = new System.Drawing.Point(705, 270);
+			this.button_VMAT.Name = "button_VMAT";
+			this.button_VMAT.Size = new System.Drawing.Size(133, 52);
+			this.button_VMAT.TabIndex = 0;
+			this.button_VMAT.Text = "VMAT";
+			this.button_VMAT.UseVisualStyleBackColor = true;
+			this.button_VMAT.Click += new System.EventHandler(this.button_VMAT_Click);
+			// 
+			// button_AutoBreast
+			// 
+			this.button_AutoBreast.Location = new System.Drawing.Point(844, 270);
+			this.button_AutoBreast.Name = "button_AutoBreast";
+			this.button_AutoBreast.Size = new System.Drawing.Size(133, 51);
+			this.button_AutoBreast.TabIndex = 1;
+			this.button_AutoBreast.Text = "AutoBreast";
+			this.button_AutoBreast.UseVisualStyleBackColor = true;
+			this.button_AutoBreast.Click += new System.EventHandler(this.button_AutoBreast_Click);
+			// 
+			// textBox_result
+			// 
+			this.textBox_result.Location = new System.Drawing.Point(126, 285);
+			this.textBox_result.Name = "textBox_result";
+			this.textBox_result.Size = new System.Drawing.Size(562, 22);
+			this.textBox_result.TabIndex = 3;
+			
+			// 
+			// status
+			// 
+			this.status.AutoSize = true;
+			this.status.ForeColor = System.Drawing.Color.Black;
+			this.status.Location = new System.Drawing.Point(123, 247);
+			this.status.Name = "status";
+			this.status.Size = new System.Drawing.Size(0, 17);
+			this.status.TabIndex = 4;
+			
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(506, 162);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			// 
+			// textConsole
+			// 
+			this.textConsole.Location = new System.Drawing.Point(705, 32);
+			this.textConsole.Multiline = true;
+			this.textConsole.Name = "textConsole";
+			this.textConsole.Size = new System.Drawing.Size(272, 232);
+			this.textConsole.TabIndex = 6;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(702, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(147, 17);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Résultat Modification :";
+		
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(9, 287);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(111, 17);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "Nom du Fichier :";
+			
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(984, 334);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textConsole);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.status);
+			this.Controls.Add(this.textBox_result);
+			this.Controls.Add(this.button_AutoBreast);
+			this.Controls.Add(this.button_VMAT);
+			this.Name = "Form1";
+			this.Text = "dcm Tool";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button_VMAT;
+		private System.Windows.Forms.Button button_AutoBreast;
+		private System.Windows.Forms.TextBox textBox_result;
+		private System.Windows.Forms.Label status;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.TextBox textConsole;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ dcmOrlam/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Windows.Forms;
+using EvilDICOM.Core;
+using EvilDICOM.Core.Element;
+using EvilDICOM.Core.Selection;
+using EvilDICOM.Core.Helpers;
+using System.IO;
+
+
+namespace dcmOrlam
+{
+	public partial class Form1 : Form
+	{
+		TextWriter _writer = null;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button_VMAT_Click(object sender, EventArgs e)
+		{
+			
+			OpenFileDialog openFileDialog1 = new OpenFileDialog();
+			
+			String path = @"vaimg101.vaonc - vil.local\DICOM\RS2VARIAN\Import\";
+
+			openFileDialog1.InitialDirectory = path;
+			openFileDialog1.Filter = "dcm Files|*.dcm";
+			openFileDialog1.Title = "Select a dcm File";
+
+			if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+					string sourceFile = openFileDialog1.FileName;
+					textBox_result.Text = sourceFile;
+					vidangeligne(sourceFile);
+					calculprescription(sourceFile);
+
+				   // status.ForeColor = System.Drawing.Color.Green;
+				   //status.Text = "Modification VMAT effectuée";
+			}
+		}
+		private void button_AutoBreast_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+			String path = @"\\vaimg101.vaonc - vil.local\DICOM\RS2VARIAN\Import\";
+
+			openFileDialog1.InitialDirectory = path;
+			openFileDialog1.Filter = "dcm Files|*.dcm";
+			openFileDialog1.Title = "Select a dcm File";
+
+			if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				string sourceFile = openFileDialog1.FileName;
+				textBox_result.Text = sourceFile;
+				vidangeligne(sourceFile);
+				calculprescription(sourceFile);
+				ajouttag(sourceFile);
+
+				//status.ForeColor = System.Drawing.Color.Red;
+				//status.Text = "Modification AutoBreast effectuée";
+			}
+		}
+
+		private void vidangeligne(String nom)
+		{
+			var dcm = DICOMObject.Read(nom);
+			var sel = new DICOMSelector(dcm);
+
+			try
+			{
+				var Vertical = sel.TableTopVerticalPosition_;
+				var Longitudinal = sel.TableTopLongitudinalPosition_;
+				var Lateral = sel.TableTopLateralPosition_;
+
+				for (int i = 0; i < Vertical.Count; i++)
+				{
+					sel.TableTopVerticalPosition_[i].Data_.Clear();
+				}
+
+				for (int j = 0; j < Longitudinal.Count; j++)
+				{
+					sel.TableTopLongitudinalPosition_[j].Data_.Clear();
+				}
+
+				for (int k = 0; k < Lateral.Count; k++)
+				{
+					sel.TableTopLateralPosition_[k].Data_.Clear();
+				}
+
+				Console.WriteLine("Les lignes 'Table TopVertical Position', 'Table Top Longitudinal Position' et 'TableTopLateralPosition' sont vide.");
+			}
+			catch (NullReferenceException)
+			{
+				MessageBox.Show("Impossible de vider les lignes.", "Erreur", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+				status.ForeColor = System.Drawing.Color.Red;
+				status.Text = "Impossible de vider les lignes ";
+			}
+
+			dcm.Write(nom);
+		}
+		private void calculprescription(String nom)
+		{
+			var dcm = DICOMObject.Read(nom);
+			var sel = new DICOMSelector(dcm);
+
+			try
+			{
+				var PrescripDose = sel.TargetPrescriptionDose.Data;
+				var DoseFraction = sel.NumberOfFractionsPlanned.Data;
+
+				var DoseParSeance = PrescripDose / DoseFraction;
+
+				Console.WriteLine("Prescription Dose : " + PrescripDose);
+				Console.WriteLine("Dose par Seance :  " + DoseFraction);
+				Console.WriteLine("Fraction de la dose : " + DoseParSeance);
+
+				var BeamDoseSpec = sel.BeamDoseSpecificationPoint_;
+
+				// nombre de dose 
+				var NbBeamDoseSpec = sel.BeamDoseSpecificationPoint_.Count;
+
+				for (int i = 0; i < BeamDoseSpec.Count; i++)
+				{
+					sel.BeamDoseSpecificationPoint_[i].Data_.Clear();
+				}
+				Console.WriteLine("Les " + sel.BeamDoseSpecificationPoint_.Count + " champs 'Beam Dose Spécifation Point' sont maintenant vides ");
+
+				var numberDose = DoseParSeance / NbBeamDoseSpec;
+				Console.WriteLine("Nombre de dose: " + numberDose);
+
+				var beamdose = sel.BeamDose_;
+				for (int i = 0; i < beamdose.Count; i++)
+				{
+					for (int k = 0; i < beamdose.Count; i++)
+					{
+						sel.BeamDose_[i].Data_[k] = numberDose;
+					}
+				}
+			}
+			catch (NullReferenceException)
+			{
+				MessageBox.Show("Impossible de calculer la dose.","Erreur",MessageBoxButtons.OKCancel,MessageBoxIcon.Error);
+				status.ForeColor = System.Drawing.Color.Red;
+				status.Text = "Calcul de Dose Impossible";
+			};
+			dcm.Write(nom);
+		}
+		private void ajouttag(String nom )
+		{
+			var dcm = DICOMObject.Read(nom);
+			var sel = new DICOMSelector(dcm);
+
+			var refDoseRate = new DecimalString
+			{
+				Tag = TagHelper.DoseRateSet,
+				Data = 400
+			};
+
+			var nbBeams = sel.NumberOfBeams.Data;
+
+			for (int i = 0; i < nbBeams; i++)
+			{
+				sel.ControlPointSequence_[i].Items[0].Add(refDoseRate);
+			}
+			dcm.Write(nom);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			_writer = new TextBoxStreamWriter(textConsole);
+			Console.SetOut(_writer);
+		}
+		
+
+		
+	}
+}
